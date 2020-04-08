dd54b7bfd0a6bfc0e1665993141e4dfd778f8cb7
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-08 17:31:09 -0400

    Add logger middleware

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const morgan = require("morgan");
+
+const userRouter = require("./users/userRouter");
 
 const server = express();
+
+//Middleware
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +14,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.originalUrl}`);
+  next();
+}
 
 module.exports = server;
